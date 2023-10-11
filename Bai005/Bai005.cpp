3be74ab43464553637a0893488327bcb609553ac
noobpyno <mlbv2005@gmail.com>
2023-10-11 08:30:38 +0700

    Vuong

--- Bai005/Bai005.cpp
+++ Bai005/Bai005.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void Nhap(int[][500], int&, int&);
-void Xuat(int[][500], int, int);
+void Nhap(int[][100], int&, int&);
+void Xuat(int[][100], int, int);
+
 
 int main()
 {
-	int a[500][500], m, n;
+	int a[100][100], m, n;
 	Nhap(a, m, n);
 	Xuat(a, m, n);
 	return 0;
 }
-void Nhap(int a[][500], int& m, int& n)
+void Nhap(int a[][100], int& m, int& n)
 {
 	cout << "Nhap so hang: ";
 	cin >> m;
@@ -24,7 +25,7 @@ void Nhap(int a[][500], int& m, int& n)
 			cin >> a[i][j];
 		}
 }
-void Xuat(int a[][500], int m, int n)
+void Xuat(int a[][100], int m, int n)
 {
 	for (int i = 0; i < m; i++)
 		for (int j = 0; j < n; j++)
