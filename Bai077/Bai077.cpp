147caff1a4095213e38806bc87f35885c4ee578d
noobpyno <mlbv2005@gmail.com>
2023-10-13 15:36:32 +0700

    .

--- Bai077/Bai077.cpp
+++ Bai077/Bai077.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 void Nhap(float[][100], int&, int&, int&);
 float LonNhat(float[][100], int, int);
-void LietKe(float[][100], int, int, int);
 
 int main()
 {
 	float a[100][100];
 	int m, n, d;
 	Nhap(a, m, n, d);
-	LietKe(a, m, n, d);
+	cout << "Gia tri lon nhat tren dong la: " << LonNhat(a, d, n);
 	return 0;
 }
 void Nhap(float a[][100], int& m, int& n, int& d)
@@ -36,11 +35,4 @@ float LonNhat(float a[][100], int d, int n)
 			if (a[d][j] > lc)
 				lc = a[d][j];
 	return lc;
-}
-void LietKe(float a[][100], int m, int n, int d)
-{
-	float lc = LonNhat(a, d, n);
-	for (int j = 0; j <= n - 1; j++)
-		if (a[d][j] == lc)
-			cout << "\nSo lon nhat tren hang la: " << a[d][j];
 }
