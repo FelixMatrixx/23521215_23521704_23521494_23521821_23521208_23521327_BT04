d04d11fb69022787c59993d50e2fbcf39cc67ef9
FelixMatrixx <phuc.pv0907.github@gmail.com>
2023-10-13 19:18:28 +0700

    037 done

--- Bai037/Bai037.cpp
+++ Bai037/Bai037.cpp
@@ -2,27 +2,30 @@
 #include <iomanip>
 using namespace std;
 
-void Nhap(float[][100], int&, int&);
+void Nhap(float[][100], int&, int&, int&);
 void Xuat(float[][100], int, int);
-float TongAm(float[][100], int, int);
+float TongDuong(float[][100], int, int, int);
 
 int main()
 {
     float b[100][100];
     int k, l;
-    Nhap(b, k, l);
+    int d;
+    Nhap(b, k, l, d);
     cout << "\nMa tran ban dau: \n";
     Xuat(b, k, l);
-    float kq = TongAm(b, k, l);
-    cout << "Tong cac gia tri am: " << kq;
+    float kq = TongDuong(b, k, l, d);
+    cout << "Tong cac gia tri duong tren dong: " << kq;
     return 0;
 }
-void Nhap(float a[][100], int& m, int& n)
+void Nhap(float a[][100], int& m, int& n, int& d)
 {
     cout << "Nhap m: ";
     cin >> m;
     cout << "Nhap n: ";
     cin >> n;
+    cout << "Nhap dong: ";
+    cin >> d;
     for (int i = 0; i <= m - 1; i++)
         for (int j = 0; j <= n - 1; j++)
         {
@@ -39,12 +42,11 @@ void Xuat(float a[][100], int m, int n)
         cout << endl;
     }
 }
-float TongAm(float a[][100], int m, int n)
+float TongDuong(float a[][100], int m, int n, int d)
 {
     float s = 0;
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++)
-            if (a[i][j] < 0)
-                s += a[i][j];
+    for (int j = 0; j < n; j++)
+            if (a[d][j] > 0)
+                s += a[d][j];
     return s;
 }
