f35580cdf1ded3a6ed300d67975217c99430fb66
FelixMatrixx <phuc.pv0907.github@gmail.com>
2023-10-14 22:11:25 +0700

    139 done

---
+++ Bai127/Bai127.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void Nhap(float[][100], int&, int&);
+void Xuat(float[][100], int, int);
+int ktCotChan(float[][100], int, int);
+int ktCotLe(float[][100], int, int);
+void SapCotTang(float[100], int, int);
+void SapCotGiam(float[][100], int, int);
+void SapCotTangGiam(float[][100], int, int);
+
+int main()
+{
+    float b[100][100];
+    int k, l;
+    Nhap(b, k, l);
+    cout << "\nMa tran ban dau: \n";
+    Xuat(b, k, l);
+    float kq = LonNhat(b, k, l);
+    cout << "Gia tri lon nhat trong ma tran: " << kq;
+    return 0;
+}
+void Nhap(float a[][100], int& m, int& n)
+{
+    cout << "Nhap m: ";
+    cin >> m;
+    cout << "Nhap n: ";
+    cin >> n;
+    for (int i = 0; i <= m - 1; i++)
+        for (int j = 0; j <= n - 1; j++)
+        {
+            cout << "Nhap a[" << i << "][" << j << "]: ";
+            cin >> a[i][j];
+        }
+}
+void Xuat(float a[][100], int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << fixed << setw(8) << setprecision(3) << a[i][j];
+        cout << endl;
+    }
+}
+int ktCotChan(float a[][100], int , int);
